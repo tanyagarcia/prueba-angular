7f634513d17b1f609ee5db02e27b525b1d407575
Tanya García <garcia.tanya0204@gmail.com>
2021-03-18 21:48:15 -0300

    Primer commit. Modificaciones par deploy heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-proyecto';
+  edadUno:number = 0;
+  edadDos:number = 0;
+  resultadoSuma:number = 0;
+  resultadoPromedio:number = 0;
+
+
+Sumar()
+{
+  this.resultadoSuma = this.edadUno + this.edadDos;
+}
+
+Promedio()
+{
+  var resultado = this.edadUno + this.edadDos;
+  this.resultadoPromedio = resultado / 2;
+}
+
 }
